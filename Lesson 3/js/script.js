c26a9ddf054aad195e713a1c4384acf1d41a0dd5
Ruslan <bodymaker@bk.ru>
2018-03-28 17:10:12 +0300

    Это третий коммит

---
+++ Lesson 3/js/script.js
@@ -0,0 +1,96 @@
+let budget,
+	nameShop,
+	time,
+	price
+
+function start() {
+	budget = prompt('Ваш бюджет на месяц?');
+
+	while (isNaN(budget) || budget == '' || budget == null) {
+		budget = prompt('Ваш бюджет на месяц?');
+	}
+
+	nameShop = prompt('Название Вашего магазина?').toUpperCase();
+	time = 19;
+	price = 5000;	
+}
+start();
+
+let mainList = {
+	listBudget: budget,
+	listNameShop: nameShop,
+	shopGoods: [],
+	employers: {},
+	open: true,
+	discount: true
+}
+
+function chooseGoods() {
+	for (let i = 0; i < 3; i++){
+		let a = prompt('Какой тип товаров будем продавать?');
+		if (typeof(a) === 'string' && typeof(a) != null && a != '' && a.length < 50) {
+			console.log('Все верно!');
+			mainList.shopGoods[i] = a;
+		} else {
+			i--;
+		}
+	}
+}
+
+chooseGoods();
+
+function workTime(time) {
+	if (time < 0){
+		console.log('Такого просто не может быть');
+	} else if (time > 8 && time < 20) {
+		console.log('Время работать');
+		} else if(time < 24) {
+			console.log('Уже слишком поздно');
+			} else {
+				console.log('В сутках только 24 часа');
+				}
+}
+workTime(18);
+
+// Дисконтная система
+let disc_price; 
+
+function discSystem() {
+	if (mainList.discount) {
+	disc_price = price * 0.8;
+	console.log('Цена со скидкой ' + disc_price);	
+	} else {
+		console.log('Цена без скидки ' + price);
+		}
+}
+discSystem();
+
+
+// Расчет дневного бюджета
+let day_budget,
+	day 
+
+function calcBudget() {
+	day = 30;
+	day_budget = budget/day;
+}
+calcBudget();
+
+
+//Найм сотрудников
+function startEmploy() {
+	for (let j = 1; j <= 4; j++) {
+		let name = prompt('Ваше имя?');
+		if (typeof(name) === 'string' && typeof(name) != null && name != '' && name.length < 100) {
+			console.log('Имена добавлены!');
+			mainList.employers[j] = name;
+		} else {
+			j--;
+		}
+	}
+}
+startEmploy();
+
+
+console.log(mainList);
+console.log('Бюджет на день (руб): ' + day_budget);
