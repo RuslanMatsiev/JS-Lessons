06838cdbb947a9cf5fcea9658f7f661974ad6ff7
Ruslan <bodymaker@bk.ru>
2018-04-07 17:03:38 +0300

    -a

---
+++ Lesson 10 - 1/js/script.js
@@ -0,0 +1,26 @@
+class createDiv {
+	constructor(height, width, bg, fontSize, textAlign) {
+		this.height = '70px';
+		this.width = '200px';
+		this.bg = '#1abc9c';
+		this.fontSize = '18px';
+		this.textAlign = 'right';
+	}
+	attributes() {
+		let div = document.createElement('div');
+
+			div.classList.add('first');
+			document.body.appendChild(div);
+			div.textContent = 'Это пробный текст';
+			console.log(div.textContent);
+
+			div.style.height = this.height;
+			div.style.width = this.width;
+			div.style.background = this.bg;
+			div.style.fontSize = this.fontSize;
+			div.style.textAlign = this.textAlign;
+	}
+}
+
+const newDiv = new createDiv();
+newDiv.attributes();
