9b93acd6608fc4d89e6d1b61b5b4f1293f608202
RuslanMatsiev <37809090+RuslanMatsiev@users.noreply.github.com>
2018-04-11 00:38:38 +0300

    Update script.js

--- Lesson 11/js/script.js
+++ Lesson 11/js/script.js
@@ -115,31 +115,34 @@ let more = document.querySelector('.more'),
 	let form = document.getElementsByClassName('main-form');
 	
 	for (let j =  0; j < form.length; j++) {
-		let form_footer = document.getElementsByTagName('form')[0],
-		    input = form[j].getElementsByTagName('input'),
-		    statusMessage = document.createElement('div');
-		    form_footer.classList.add('main-form');
-		    statusMessage.classList.add('status');
+		
+		let form_footer = document.getElementsByTagName('form')[0];
+			form_footer.classList.add('main-form');
+		
+		let input = form[j].getElementsByTagName('input'),
+			statusMessage = document.createElement('div');
+		
+			statusMessage.classList.add('status');
 
-		    form[j].addEventListener('submit', function(event) {
-		    event.preventDefault();
-		    form[j].appendChild(statusMessage);
+			form[j].addEventListener('submit', function(event) {
+			event.preventDefault();
+			form[j].appendChild(statusMessage);
 
-		    // AJAX
-		    let request = new XMLHttpRequest();
-		    request.open("POST", 'server.php');
-		    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoder");
-    	            
-	            let formData = new FormData(form[j]);
-		    request.send(formData);
-   		    
-		    request.onreadystatechange = function() {
-			if (request.readyState < 4) {
-				statusMessage.innerHTML = message.loading;
-				} else if (request.readyState === 4) {
-					if (request.status == 200 && request.status < 300) {
-						statusMessage.innerHTML = message.success;
-						//Добавляем контент на страницу
+			// AJAX
+			let request = new XMLHttpRequest();
+			request.open("POST", 'server.php');
+			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoder");
+
+			let formData = new FormData(form[j]);
+			request.send(formData);
+
+			request.onreadystatechange = function() {
+				if (request.readyState < 4) {
+						statusMessage.innerHTML = message.loading;
+					} else if (request.readyState === 4) {
+						if (request.status == 200 && request.status < 300) {
+							statusMessage.innerHTML = message.success;
+							//Добавляем контент на страницу
 						}
 						else{
 							statusMessage.innerHTML = message.failure;
@@ -149,6 +152,6 @@ let more = document.querySelector('.more'),
 				for (let i =  0; i < input.length; i++) {
 					input[i].value = '';
 					//Очищаем поля ввода
-		        	}
-		    });
+				}
+			});
 	}
