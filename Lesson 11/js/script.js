3120c3772d767a2cb16b630e6f27799695e84981
RuslanMatsiev <37809090+RuslanMatsiev@users.noreply.github.com>
2018-04-11 18:21:37 +0300

    Update script.js

--- Lesson 11/js/script.js
+++ Lesson 11/js/script.js
@@ -105,18 +105,16 @@ let more = document.querySelector('.more'),
 	}
 
 
-// Форма
-let message = {
-	loading: 'Загрузка...',
-	success: 'Спасибо! Скоро мы с Вами свяжемся',
-	failure: 'Что-то пошло не так...'
-};
-
-let form = document.getElementsByClassName('main-form');
-
-for (let j =  0; j < form.length; j++) {
+	// Форма
+	let message = {
+		loading: 'Загрузка...',
+		success: 'Спасибо! Скоро мы с Вами свяжемся',
+		failure: 'Что-то пошло не так...'
+	};
 	
-<<<<<<< HEAD
+	let form = document.getElementsByClassName('main-form'),
+		input = form.getElementsByTagName('input');
+	
 	for (let j =  0; j < form.length; j++) {
 		let form_footer = document.getElementsByTagName('form')[0],
 		    input = form.getElementsByTagName('input'),
@@ -137,35 +135,12 @@ for (let j =  0; j < form.length; j++) {
 		    request.send(formData);
    		    
 		    request.onreadystatechange = function() {
-=======
-	let form_footer = document.getElementsByTagName('form')[0];
-	form_footer.classList.add('main-form');
-		
-	let input = form[j].getElementsByTagName('input'),
-	statusMessage = document.createElement('div');
-	statusMessage.classList.add('status');
-
-	form[j].addEventListener('submit', function(event) {
-		event.preventDefault();
-		form[j].appendChild(statusMessage);
-
-		// AJAX
-		let request = new XMLHttpRequest();
-		request.open("POST", 'server.php');
-		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoder");
-
-		let formData = new FormData(form[j]);
-		request.send(formData);
-
-		request.onreadystatechange = function() {
->>>>>>> 56be843d9921f274956458a12ba19f0388db4d82
 			if (request.readyState < 4) {
 				statusMessage.innerHTML = message.loading;
 				} else if (request.readyState === 4) {
 					if (request.status == 200 && request.status < 300) {
 						statusMessage.innerHTML = message.success;
 						//Добавляем контент на страницу
-<<<<<<< HEAD
 						}
 						else{
 							statusMessage.innerHTML = message.failure;
@@ -178,17 +153,3 @@ for (let j =  0; j < form.length; j++) {
 		        	}
 		    });
 	}
-=======
-					}
-					else {
-						statusMessage.innerHTML = message.failure;
-						}
-					}
-		};
-		for (let i =  0; i < input.length; i++) {
-			input[i].value = '';
-			//Очищаем поля ввода
-		}
-	});
-}
->>>>>>> 56be843d9921f274956458a12ba19f0388db4d82
